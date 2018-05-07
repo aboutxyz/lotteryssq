e3709da534cec8e10d3fc0163ae12d594a8f6f09
aboutxyz <haishangmuxucao@dlmu.edu.cn>
2018-05-07 22:51:29 +0800

    a little

--- assq.py
+++ assq.py
@@ -1,4 +1,5 @@
-#coding:utf-8
+#coding=utf-8
+from __future__ import unicode_literals
 
 import MySQLdb
 import numpy as np
@@ -14,7 +15,14 @@ ss3 = Series(data.red3)
 ss4 = Series(data.red5)
 ss5 = Series(data.red5)
 ss6 = Series(data.red6)
-print pd.concat([ss1,ss2]).value_counts().values
-print sum(pd.concat([ss1,ss2]).value_counts().values)
+
+from pyecharts import Bar
+
+attr = pd.concat([ss1,ss2,ss3,ss4,ss5,ss6]).value_counts(sort = False).index.tolist()
+v1 = pd.concat([ss1,ss2,ss3,ss4,ss5,ss6]).value_counts(sort = False).values.tolist()
+bar = Bar(u"双色球")
+bar.add(u"红球", attr, v1)
+bar.render()
+
 
 db.close()
