359f64b352ab3f67349473da0b2671da5054f974
aboutxyz <haishangmuxucao@dlmu.edu.cn>
2018-05-07 16:52:25 +0800

    5.7

--- assq.py
+++ assq.py
@@ -3,13 +3,18 @@
 import MySQLdb
 import numpy as np
 import pandas as pd 
+from pandas import Series
 
 db = MySQLdb.connect(host='127.0.0.1',user='root',passwd='900502',db='ssq',port=3306,charset='utf8')
 cursor=db.cursor()
-data = pd.read_sql(r"select * from ssq where blue=16",con = db)
-print list(data.red1)
-
-
-
+data = pd.read_sql(r"select * from ssq",con = db)
+ss1 = Series(data.red1)
+ss2 = Series(data.red2)
+ss3 = Series(data.red3)
+ss4 = Series(data.red5)
+ss5 = Series(data.red5)
+ss6 = Series(data.red6)
+print pd.concat([ss1,ss2]).value_counts().values
+print sum(pd.concat([ss1,ss2]).value_counts().values)
 
 db.close()
